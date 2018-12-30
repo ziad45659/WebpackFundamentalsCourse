66f76c537165463eaf120213075b3a541cd0d859
ziad <ziad400@gmail.com>
2018-12-30 07:49:06 +0100

    Upgraded webpack to version 4.27.1

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,4 @@
 console.log('logging from utils.js');
+const x = () => {
+    console.log("xxx ");
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+//var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     context: path.resolve('js'), // webpack will look for the entry files (utils and app) inside the js directory
@@ -11,41 +13,72 @@ module.exports = {
         filename: "bundle.js" // the file that we create
     },
     plugins: [
-        new ExtractTextPlugin("styles.css")
+        //new ExtractTextPlugin("styles.css")
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
     ],
     devServer: {
         contentBase: 'public'
     },
     watch: true, // to enter watch mode
     module: {
-        preLoaders: [
+        rules: [
+/*
             {
-                test: /\.js$/,
+                test: /.js/,
+                enforce: 'pre',
                 exclude: /node_modules/,
-                loader: "jshint-loader"
-            }
-        ],
-        loaders: [
+                use: [
+                    {
+                        loader: `jshint-loader`,
+                        options: {}
+                    }
+                ]
+            },*/
             { // babel loader
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                // loader: "style-loader!css-loader" // if used like this, then css is loaded into the html and not to a separate file
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
-            {
+            /*{
+                test: /\.css$/,
+                 //exclude: /node_modules/,  // removed for the mini css extract plugin
+                //loader: "style-loader!css-loader" // if used like this, then css is loaded into the html and not to a separate file
+                //loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+
+
+            },*/
+            /*{
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                //loader: "style-loader!css-loader!sass-loader"
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
-            }
+                loader: "style-loader!css-loader!sass-loader"
+                //loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+                /!*use: [
+                    MiniCssExtractPlugin.loader,
+                    "sass-loader"
+                ]*!/
+            }*/
         ]
     },
     resolve: {
-        extensions: ['', '.js', '.es6'] // login or login.js or login.es6
+        extensions: ['.js', '.es6'] // login or login.js or login.es6
     }
 };
